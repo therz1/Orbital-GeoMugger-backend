fc2e658b6cea9e828e03147480cbe188bdfe5ad4
hz <huizhongsim@gmail.com>
2026-06-26 00:02:03 +0800

    update calculation of jaccard similarity to include weighted component

--- recommendation_spot.py
+++ recommendation_spot.py
@@ -24,7 +24,7 @@ def get_user_with_preferred_tag():
             res[user_id] = user_data['preferredTags']
     return res
 
-#return dict{string: List<String>} location_id: List<tagName>
+#return dict{string: dict {string: int}} location_id: dict {tag name; tag count}>
 def get_location_with_tags():
     location_grp = db.collection("locations").stream()
     res = {}
@@ -33,25 +33,34 @@ def get_location_with_tags():
         location_data = location.to_dict()
         location_id = location.id
         all_tags_dict = location_data.get("allTags", {})
-        #might want to include the tagCount too for weighted calculation of jaccard sim
-        tagName = list(all_tags_dict.keys())
+        
+        tag_count = {}
+        for tagName , tagInfo in all_tags_dict.items():
+            tag_count[tagName] = int(tagInfo.get("count", 1))
+   
         if location_id not in res:
-            res[location_id] = tagName
+            res[location_id] = tag_count
     return res
 
 
 #return double jaccard similarity between two sets.
-def jaccard_sim(user, loc):
-    loc_set = set(loc)
+def weighted_jaccard_sim(user, tag_count):
+    loc_set = set(tag_count.keys())
     user_set = set(user)
     
+    common_tag_set = loc_set.intersection(user_set)
     numerator = len(loc_set.intersection(user_set))
     denominator = len(loc_set.union(user_set))
     
     if (denominator == 0):
         return 0.0
+
+#updated jaccard similarity calculation to include weighted component
     else:
-        return numerator/denominator
+        multiplier = 0.05
+        weight = sum(tag_count.get(tag) for tag in common_tag_set)
+        weight_multiplier = (1 + (weight * multiplier))
+        return numerator/denominator * weight_multiplier
 
 
 #return dict{string: List<string>} userId: List<locationId> sorted based on descending jaccard similarity score
@@ -66,13 +75,13 @@ def update_recommended_spots_in_firestore():
     #sort location id based on jaccard similarity
     for user_id in user_data.keys():
         res[user_id] = sorted(location_data.keys(),
-                              key = lambda x: jaccard_sim(user_data[user_id], location_data[x]), 
+                              key = lambda x: weighted_jaccard_sim(user_data[user_id], location_data[x]), 
                               reverse=True)[:10]
         #limit size to 10 elements to prevent taking up too much space
         db.collection('user').document(user_id).set(
             {"recommendedSpots": res[user_id],
             "updatedAt": firestore.SERVER_TIMESTAMP}, merge= True)
-    
 
-    
+
+
 update_recommended_spots_in_firestore()
